4a93131410298ada8a5f149fc061f335db171159
badolusa <bangladeshiqa@gmail.com>
2020-12-21 16:03:27 -0500

    added package and class. I added some stuff in main method just for example

--- src/nonePrimitiveDataType/PrintHelloWorld.java
+++ src/nonePrimitiveDataType/PrintHelloWorld.java
@@ -2,6 +2,11 @@ package nonePrimitiveDataType;
 
 public class PrintHelloWorld {
     public static void main(String[]args){
-        System.out.println("hello ripon");
+        System.out.println("Briarwood\n");
+        System.out.println("New York\n");
+        System.out.println("United States Of Anerica\n");
+        System.out.println("A Real Bangladeshi");
+
+
     }
 }
